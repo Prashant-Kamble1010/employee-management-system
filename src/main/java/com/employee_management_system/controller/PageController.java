9ea552fb0cdc66e12888f427a2ea49ac53b4c652
Prashant Kamble <prashant.kamble24@pccoepune.org>
2026-07-15 01:22:12 +0530

    maked controller of signup and sign in

--- src/main/java/com/employee_management_system/controller/PageController.java
+++ src/main/java/com/employee_management_system/controller/PageController.java
@@ -5,25 +5,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-    @GetMapping("/")
-    public String index() {
-        return "Home/index";
-    }
+//    @GetMapping("/")
+//    public String index() {
+//        return "Home/index";
+//    }
+//
+//    @GetMapping("/signup")
+//    public String signup() {
+//        return "Home/signup";
+//    }
+//
+//    @GetMapping("/signin")
+//    public String signin() {
+//        return "Home/signin";
+//    }
 
-    @GetMapping("/signup")
-    public String signup() {
-        return "Home/signup";
-    }
-
-    @GetMapping("/signin")
-    public String signin() {
-        return "Home/signin";
-    }
-
-    @GetMapping("/dashboard")
-    public String dashboard() {
-        return "Home/dashboard";
-    }
+//    @GetMapping("/dashboard")
+//    public String dashboard() {
+//        return "Home/dashboard";
+//    }
 
     @GetMapping("/aboutus")
     public String about() {
