9ec0f8f63cd08f8c8d29c2fdf5455cdf69f8e773
Prashant Kamble <prashant.kamble24@pccoepune.org>
2026-07-14 23:18:36 +0530

    add HOme COntroller

---
+++ src/main/java/com/employee_management_system/controller/PageController.java
@@ -0,0 +1,37 @@
+package com.employee_management_system.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Home/index";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "Home/signup";
+    }
+
+    @GetMapping("/signin")
+    public String signin() {
+        return "Home/signin";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "Home/dashboard";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "Home/aboutus";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "Home/contactus";
+    }
+}
